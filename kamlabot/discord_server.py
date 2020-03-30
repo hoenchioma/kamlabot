c5ad467e7c91784fd228f84e0b2e1dca91791a1d
Raheeb Hassan <raheeb998@gmail.com>
2020-03-30 18:11:10 +0600

    Fixed the mentions issue
    - removes mention of bot
    - replaces mentions of other users with respective names

--- kamlabot/discord_server.py
+++ kamlabot/discord_server.py
@@ -13,6 +13,7 @@ client = discord.Client()
 # set logging level to info
 logging.basicConfig(level=logging.INFO)
 
+
 @client.event
 async def on_ready():
     logging.info("[DISCORD] Discord bot server started ...")
@@ -28,19 +29,31 @@ async def on_message(msg):
     # we do not want the bot to reply to itself
     if msg.author == client.user:
         return
-    
+
     response = None
 
     # message starts with keyword
     if msg.content.startswith(KEYWORD):
-        response = _process_msg(msg.content[len(KEYWORD):], msg.author)
+        # remove the keyword prefix
+        txt = msg.content[len(KEYWORD):]
+
+        # replace other mentions with names
+        txt = _replace_mentions(txt, msg.mentions)
+
+        response = _process_msg(txt.strip(), msg.author)
 
     # bot is mentioned in message
     if client.user.mentioned_in(msg):
-        response = _process_msg(
-            discord.utils.escape_mentions(msg.content),
-            msg.author
-        )
+        txt = msg.content
+
+        # remove mentions of the bot
+        txt = txt.replace(f"<@{client.user.id}>", "")
+        txt = txt.replace(f"<@!{client.user.id}>", "")
+
+        # replace other mentions with names
+        txt = _replace_mentions(txt, msg.mentions)
+
+        response = _process_msg(txt.strip(), msg.author)
 
     if response:  # response isn't empty
         await channel.send(response)
@@ -52,6 +65,14 @@ def _process_msg(txt, author):
     response = get_bot_response(author.id, text=txt)
     # mention the user sending the message
     return author.mention + " " + response
+
+
+def _replace_mentions(msg, mentions):
+    """Convert mentions to corresponding names"""
+    for member in mentions:
+        msg = msg.replace(f"<@{member.id}>", member.name)
+        msg = msg.replace(f"<@!{member.id}>", member.name)
+    return msg
 
 
 def main():
