c59000f305c54f3a7bda5184f8de0fabe2627e1f
Raheeb Hassan <raheeb998@gmail.com>
2020-04-28 14:49:13 +0600

    Updated database.py
    - Added get_others
    - Added check to remove leading slashes

--- kamlabot/misc/database.py
+++ kamlabot/misc/database.py
@@ -16,13 +16,26 @@ def get_data(path: str) -> OrderedDict:
 
 def get_responses(path: str = "") -> OrderedDict:
     """get an dict containing all app responses (corresponding to various message scenarious)"""
+    # remove any leading slashes
+    if len(path) >= 1 and path[0] == '/':
+        path = path[1:]
     return get_data('responses' + '/' + path)
 
 def get_info(path: str = "") -> OrderedDict:
     """get a dict containing all app info (links, calendar, etc.) """
+    # remove any leading slashes
+    if len(path) >= 1 and path[0] == '/':
+        path = path[1:]
     return get_data('info' + '/' + path)
+
+def get_others(path: str = "") -> OrderedDict:
+    """get a dict containing other info (joke-api, etc)"""
+    # remove any leading slashes
+    if len(path) >= 1 and path[0] == '/':
+        path = path[1:]
+    return get_data('others' + '/' + path)
 
 
 if __name__ == "__main__":
-    pass
+    print(json.dumps(get_data(''), indent=2))
 
